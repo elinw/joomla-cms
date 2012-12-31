baad8fbdfab806467522a0493e64df861f2ba068
elinw <elin.waring@gmail.com>
2012-12-31 18:23:03 -0500

    Work on front end tag display.

--- components/com_contact/models/contact.php
+++ components/com_contact/models/contact.php
@@ -95,11 +95,13 @@ class ContactModelContact extends JModelForm
 	}
 
 	/**
-	 * Gets a list of contacts
-	 * @param array
+	 * Gets a contact
+	 *
+	 * @param integer $pk  Id for the contact
+	 *
 	 * @return mixed Object or null
 	 */
-	public function &getItem($pk = null)
+	public function getItem($pk = null)
 	{
 		$pk = (!empty($pk)) ? $pk : (int) $this->getState('contact.id');
 
@@ -183,6 +185,9 @@ class ContactModelContact extends JModelForm
 				$registry = new JRegistry;
 				$registry->loadString($data->metadata);
 				$data->metadata = $registry;
+
+				$data->tags = new JTagsHelper;
+				$data->tags->getItemTags('com_contact.contact' . $data->id);
 
 				// Compute access permissions.
 				if ($access = $this->getState('filter.access')) {
--- components/com_newsfeeds/views/newsfeed/view.html.php
+++ components/com_newsfeeds/views/newsfeed/view.html.php
@@ -197,6 +197,9 @@ class NewsfeedsViewNewsfeed extends JViewLegacy
 		}
 		$this->print = $print;
 
+		$item->tags = new JTagsHelper;
+		$item->tags->getItemTags('com_newsfeeds.newsfeed.' . $this->item->id); 
+
 		$this->_prepareDocument();
 
 		parent::display($tpl);
@@ -218,8 +221,13 @@ class NewsfeedsViewNewsfeed extends JViewLegacy
 		// Because the application sets a default page title,
 		// we need to get it from the menu item itself
 		$menu = $menus->getActive();
+<<<<<<< Upstream, based on master
 		if ($menu)
 		{
+=======
+
+		if ($menu) {
+>>>>>>> d2666a1 Work on front end tag display.
 			$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
 		}
 		else
