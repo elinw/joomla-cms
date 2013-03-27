ee98f855d5a0345c7fd363c74f68f84e778ed17d
Elin Waring <elin.waring@gmail.com>
2013-03-27 15:20:47 -0400

    Work on content plugins in tags

--- components/com_tags/views/tag/view.html.php
+++ components/com_tags/views/tag/view.html.php
@@ -69,6 +69,28 @@ class TagsViewTag extends JViewLegacy
 				$itemElement->params->merge($temp);
 				$itemElement->params = (array) json_decode($itemElement->params);
 			}
+
+			// For some plugins.
+			!empty($itemElement->core_body)? $itemElement->text = $itemElement->core_body : $itemElement->text = null;
+
+			$dispatcher = JEventDispatcher::getInstance();
+
+			JPluginHelper::importPlugin('content');
+			$results = $dispatcher->trigger('onContentPrepare', array ('com_tags.tag', &$itemElement, &$itemElement->core_params, 0));
+
+			$results = $dispatcher->trigger('onContentAfterTitle', array('com_tags.tag', &$itemElement, &$itemElement->core_params, 0));
+			$this->event->afterDisplayTitle = trim(implode("\n", $results));
+
+			$results = $dispatcher->trigger('onContentBeforeDisplay', array('com_tags.tag', &$itemElement, &$itemElement->core_params, 0));
+			$this->event->beforeDisplayContent = trim(implode("\n", $results));
+
+			$results = $dispatcher->trigger('onContentAfterDisplay', array('com_tags.tag', &$itemElement, &$itemElement->core_params, 0));
+			$this->event->afterDisplayContent = trim(implode("\n", $results));
+
+			if ($itemElement->text)
+			{
+				$itemElement->core_body = $itemElement->text;
+			}
 		}
 
 		$this->state      = &$state;
