9ba856c2bc606ba460f70905361d5133e12a6fe5
Elin Waring <elin.waring@gmail.com>
2013-10-12 17:41:28 -0400

    Updates

--- tests/unit/schema/ddl.sql
+++ tests/unit/schema/ddl.sql
@@ -378,8 +378,7 @@ CREATE TABLE `jos_postinstall_messages` (
   `condition_file` TEXT NOT NULL DEFAULT '',
   `condition_method` TEXT NOT NULL DEFAULT NULL,
   `version_introduced` TEXT NOT NULL DEFAULT '3.2.0',
-  `enabled` INTEGER NOT NULL DEFAULT 1,
-  PRIMARY KEY (`postinstall_message_id`)
+  `enabled` INTEGER NOT NULL DEFAULT 1,)
 )
 -- --------------------------------------------------------
 
@@ -558,7 +557,7 @@ CREATE INDEX `idx_ucm_content_core_type_id` ON `jos_ucm_content` (`core_type_id`
 --
 
 CREATE TABLE IF NOT EXISTS `jos_ucm_history` (
-  `version_id` INTEGER unsigned NOT NULL AUTO_INCREMENT,
+  `version_id` INTEGER unsigned NOT NULL  PRIMARY KEY AUTOINCREMENT,
   `ucm_item_id` INTEGER unsigned NOT NULL,
   `ucm_type_id` INTEGER unsigned NOT NULL,
   `version_note` TEXT NOT NULL DEFAULT '',
@@ -568,9 +567,9 @@ CREATE TABLE IF NOT EXISTS `jos_ucm_history` (
   `sha1_hash` TEXT NOT NULL DEFAULT '',
   `version_data` TEXT NOT NULL COMMENT,
   `keep_forever` INTEGER NOT NULL DEFAULT '0',
-  PRIMARY KEY (`version_id`),
-  KEY `idx_ucm_item_id` (`ucm_type_id`,`ucm_item_id`),
-  KEY `idx_save_date` (`save_date`)
+
+/*  KEY `idx_ucm_item_id` (`ucm_type_id`,`ucm_item_id`),
+  KEY `idx_save_date` (`save_date`)*/
 )
 -- --------------------------------------------------------
 
